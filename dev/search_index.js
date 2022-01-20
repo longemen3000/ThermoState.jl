b0887d189de9f277a626ce69f94ed7c4bd18345c
Documenter.jl <documenter@juliadocs.github.io>
2022-01-20 23:52:08 +0000

    build based on f62fb89

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Chemicals","category":"page"},{"location":"#Chemicals","page":"Home","title":"Chemicals","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Chemicals]","category":"page"}]
+}
